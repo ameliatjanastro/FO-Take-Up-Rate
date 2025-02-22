8bb495bec1e1f40b84852299218423d96c11af95
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-02-22 15:35:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -16,19 +16,19 @@ if discount_sales_file and discount_price_file and normal_sales_file:
     normal_sales = pd.read_csv(normal_sales_file, parse_dates=["Date"])
     
     # Aggregate sales data
-    discount_grouped = discount_sales.groupby(["Product ID", "Hub ID"]).agg(
+    discount_grouped = discount_sales.groupby(["Product ID", "Hub ID Fulfilled"]).agg(
         qty_sold=("Qty sold Discounted Price", "sum"),
         discount_days=("Date", "nunique")
     ).reset_index()
     
-    normal_grouped = normal_sales.groupby(["Product ID", "Hub ID"]).agg(
+    normal_grouped = normal_sales.groupby(["Product ID", "Hub ID Fulfilled"]).agg(
         qty_sold=("Total Qty Sold", "sum"),
         non_discount_days=("Date", "nunique")
     ).reset_index()
     
     # Merge Data
     df = discount_grouped.merge(discount_prices, on="Product ID", how="left")
-    df = df.merge(normal_grouped, on=["Product ID", "Hub ID"], how="left")
+    df = df.merge(normal_grouped, on=["Product ID", "Hub ID Fulfilled"], how="left")
     
     # Calculate Daily Sales Rates
     df["discounted_sales_rate"] = df["qty_sold_x"] / df["discount_days"]
@@ -39,7 +39,7 @@ if discount_sales_file and discount_price_file and normal_sales_file:
     df["discount_percentage"] = round(df["Flushout Discount (IDR)"] / df["Price"]) * 100
     # Display Results
     st.subheader("Results")
-    st.dataframe(df[["Product ID", "Hub ID", "take_up_rate","discount_percentage"]])
+    st.dataframe(df[["Product ID", "Hub ID Fulfilled", "take_up_rate","discount_percentage"]])
     
     # Option to Download Processed Data
     st.download_button("Download Results as CSV", df.to_csv(index=False), "take_up_rate_results.csv", "text/csv")
